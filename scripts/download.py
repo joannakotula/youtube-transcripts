c8dfd0949e07d263fe78f6388e7fc9a3ac1a0cba
Joanna Kotuła <joanna@kotula.net.pl>
2022-08-16 22:05:32 +0200

    prepared data for translation

---
+++ scripts/download.py
@@ -0,0 +1,27 @@
+from youtube_transcript_api import YouTubeTranscriptApi
+from smart_text_formatter import SmartTextFormatter
+import copy
+import yaml
+
+video_id = 'GNSedMOH9bg'
+
+transcript_list = YouTubeTranscriptApi.list_transcripts(video_id)
+
+transcript = transcript_list.find_transcript(['en'])
+
+def create_translation_list(orig_lang, original_transcript, pl_transcript):
+    ret = copy.deepcopy(original_transcript)
+    for i, orig in enumerate(ret):
+        pl = pl_transcript[i]
+        orig_text = orig['text']
+        pl_text = pl['text']
+        orig['text'] = {
+            orig_lang: orig_text.strip().replace('\n', ' '),
+            'pl': pl_text.strip().replace('\n', ' ')
+        }
+    return ret
+
+translation_list = create_translation_list('en', transcript.fetch(), transcript.translate('pl').fetch())
+
+print(yaml.dump(translation_list, allow_unicode=True))
+
