e4389a20569fdc2f63ce52bcc847b539f68f62dc
Joanna Kotuła <joanna@kotula.net.pl>
2022-09-06 23:45:07 +0200

    script to download transcript data from definition

--- scripts/download.py
+++ scripts/download.py
@@ -1,13 +1,36 @@
+import sys
 from youtube_transcript_api import YouTubeTranscriptApi
 from smart_text_formatter import SmartTextFormatter
 import copy
 import yaml
+import argparse
+from urllib.parse import urlparse, parse_qs
+import logging
+from pathlib import Path
 
-video_id = 'GNSedMOH9bg'
+MAIN_LANGUAGE='pl'
 
-transcript_list = YouTubeTranscriptApi.list_transcripts(video_id)
 
-transcript = transcript_list.find_transcript(['en'])
+parser = argparse.ArgumentParser(description='Download transcript data for definition file')
+parser.add_argument('--definition', '-d', type=argparse.FileType('r'), required=True)
+parser.add_argument('--output', '-o', type=str, required=True)
+
+# method based on: https://stackoverflow.com/a/54383711
+def get_yt_id(url):
+    # Examples:
+    # - http://youtu.be/SA2iWivDJiE
+    # - http://www.youtube.com/watch?v=_oPAwA_Udwc&feature=feedu
+    # - http://www.youtube.com/embed/SA2iWivDJiE
+    # - http://www.youtube.com/v/SA2iWivDJiE?version=3&amp;hl=en_US
+    query = urlparse(url)
+    if query.hostname == 'youtu.be': return query.path[1:]
+    if query.hostname in {'www.youtube.com', 'youtube.com', 'music.youtube.com'}:
+        if query.path == '/watch': return parse_qs(query.query)['v'][0]
+        if query.path[:7] == '/watch/': return query.path.split('/')[1]
+        if query.path[:7] == '/embed/': return query.path.split('/')[2]
+        if query.path[:3] == '/v/': return query.path.split('/')[2]
+   # returns None for invalid YouTube url
+
 
 def create_translation_structure(orig_lang, original_transcript, pl_transcript):
     ret = copy.deepcopy(original_transcript)
@@ -24,7 +47,27 @@ def create_translation_structure(orig_lang, original_transcript, pl_transcript):
         'content': ret
     }
 
-translation_list = create_translation_structure('en', transcript.fetch(), transcript.translate('pl').fetch())
+def prepare_transcripts(url, original_language):
+    video_id = get_yt_id(url)
+    if not video_id:
+        logging.error(f"Invalid url: {url}")
+        exit(123)
 
-print(yaml.dump(translation_list, allow_unicode=True, sort_keys=False))
+    transcript_list = YouTubeTranscriptApi.list_transcripts(video_id)
 
+    transcript = transcript_list.find_transcript([original_language])
+    # TODO: what if original_language = MAIN_LANGUAGE?
+    return create_translation_structure(original_language, transcript.fetch(), transcript.translate(MAIN_LANGUAGE).fetch())
+    
+
+args = parser.parse_args()
+
+if Path(args.output).exists():
+    logging.warning(f"File {args.output} already exists, skipping")
+    exit()
+
+definition = yaml.safe_load(args.definition)
+transcripts_data = prepare_transcripts(definition['url'], definition['original_language'])
+
+with open(args.output, 'w') as f:
+    f.write(yaml.dump(transcripts_data, allow_unicode=True, sort_keys=False))
