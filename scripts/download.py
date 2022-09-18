341bde27c2a0a9143d00c60d766a213280df20fe
Joanna Kotuła <joanna@kotula.net.pl>
2022-09-18 22:30:56 +0200

    extracted get_yt_id to separate file

--- scripts/download.py
+++ scripts/download.py
@@ -2,9 +2,9 @@ from youtube_transcript_api import YouTubeTranscriptApi
 import copy
 import yaml
 import argparse
-from urllib.parse import urlparse, parse_qs
 import logging
 from pathlib import Path
+from youtube import get_yt_id
 
 MAIN_LANGUAGE='pl'
 
@@ -12,22 +12,6 @@ MAIN_LANGUAGE='pl'
 parser = argparse.ArgumentParser(description='Download transcript data for definition file')
 parser.add_argument('--definition', '-d', type=argparse.FileType('r'), required=True)
 parser.add_argument('--output', '-o', type=str, required=True)
-
-# method based on: https://stackoverflow.com/a/54383711
-def get_yt_id(url):
-    # Examples:
-    # - http://youtu.be/SA2iWivDJiE
-    # - http://www.youtube.com/watch?v=_oPAwA_Udwc&feature=feedu
-    # - http://www.youtube.com/embed/SA2iWivDJiE
-    # - http://www.youtube.com/v/SA2iWivDJiE?version=3&amp;hl=en_US
-    query = urlparse(url)
-    if query.hostname == 'youtu.be': return query.path[1:]
-    if query.hostname in {'www.youtube.com', 'youtube.com', 'music.youtube.com'}:
-        if query.path == '/watch': return parse_qs(query.query)['v'][0]
-        if query.path[:7] == '/watch/': return query.path.split('/')[1]
-        if query.path[:7] == '/embed/': return query.path.split('/')[2]
-        if query.path[:3] == '/v/': return query.path.split('/')[2]
-   # returns None for invalid YouTube url
 
 
 def create_translation_structure(orig_lang, original_transcript, pl_transcript):
